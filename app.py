c3078af9d5b3a8e30db99825db52e75af6f84d29
Amey Darokar <129416212+Amey-sd@users.noreply.github.com>
2024-11-11 01:59:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -77,4 +77,4 @@ def processed_file(filename):
     return send_from_directory(PROCESSED_FOLDER, filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000,debug=True)
